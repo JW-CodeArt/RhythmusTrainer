ba373e7e93f5b61f6668c78be037a3f341b9594e
Jannik Weber <jannik.matteo.weber@gmail.com>
2026-01-28 19:20:35 +0100

    11th iteration

--- api/server.py
+++ api/server.py
@@ -3,10 +3,13 @@ import logging
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
-app = Flask(__name__)
+app = Flask(__name__,
+    template_folder="../templates",
+    static_folder="../static",
+    static_url_path="/static")
 
 @app.route("/")
 def hello():
-    return render_template("../trainer.html")
+    return render_template("trainer.html")
 
 #app.run(debug=False)
